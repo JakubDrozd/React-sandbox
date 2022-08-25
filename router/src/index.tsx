e616a9c4dd0bce8dc9d68c559788e76f099abec7
Veanty <plmulticraftpl@gmail.com>
2022-08-25 17:22:59 +0200

    Refactor: update the render to render nav

--- router/src/index.tsx
+++ router/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { RouteSwitch } from './RouteSwitch';
+import { Nav } from './Nav';
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <RouteSwitch></RouteSwitch>
+    <Nav></Nav>
   </React.StrictMode>
 );
 
