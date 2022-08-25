c532720d243db50e35adeb849abda23cd2f7ff7c
Veanty <plmulticraftpl@gmail.com>
2022-08-25 18:33:11 +0200

    Feat: add new routes

--- router/src/App.tsx
+++ router/src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import {Nav} from "./Nav"
+import {Shop} from "./Shop"
+import {About} from "./About"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import "./App.css"
 
 function App(){
   return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/about" element={<About></About>}></Route>
+            <Route path="/shop" element={<Shop></Shop>}></Route>
+          </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+function Home(){
+  return(
     <div>
-      <h1>Hello from App</h1>
+      <h1>Home Page</h1>
     </div>
   )
 }
