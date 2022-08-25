5ed0457d67738de06ea8d9fbc0e013ffcfb9c1ce
Veanty <plmulticraftpl@gmail.com>
2022-08-25 18:50:17 +0200

    Feat: add new way of displaying bundles and items, add links to every item

--- router/src/Nav.tsx
+++ router/src/Nav.tsx
@@ -1,16 +1,22 @@
 import App from "./App";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
 import { Profile } from "./Profile";
 import { ProfileView } from "./ProfileView";
 import { NotFound } from "./NotFound";
+import {Link} from "react-router-dom"
 
 export function Nav(){
+
+    const navStyle = {
+        color: "white",
+        textDecoration: "none"
+    }
+
     return(
-        <nav>
+        <nav className="nav">
             <h3>Logo</h3>
-            <ul>
-                <li>About</li>
-                <li>Shop</li>
+            <ul className="nav-links">
+                <Link style={navStyle} to="/about"><li>About</li></Link>
+                <Link style={navStyle} to="/shop"><li>Shop</li></Link>
             </ul>
         </nav>
     )
