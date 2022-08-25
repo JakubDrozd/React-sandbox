0f2f29add3cb65412cc86235f0f964b62edc398c
Veanty <plmulticraftpl@gmail.com>
2022-08-25 18:50:40 +0200

    Feat: add new way of displaying bundles and items, add links to every item

--- router/src/Shop.js
+++ router/src/Shop.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export function Shop() {
   useEffect(() => {
@@ -18,8 +19,15 @@ export function Shop() {
   return (
     <div className="container">
       {items.map((item) => (
-        <div className="card">
-          <h3 className="item-name">Name: {item.devName}</h3>
+        <div className="card" key={item.offerId}>
+          <h3 className="item-name">Items: </h3>
+          <h3 className="item-name" key={item.id}>
+            {item.items.map((item) => (
+              <Link to={`shop/${item.id}`}>
+                <li>{item.name}</li>
+              </Link>
+            ))}
+          </h3>
           <h4 className="item-price">Price: {item.regularPrice}</h4>
           <div className="giftable">
             {item.giftable ? <h4>Giftable</h4> : <h4>Not Giftable</h4>}
