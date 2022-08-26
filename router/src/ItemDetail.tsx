1283c82ae2839391220e14a76e554e2071a91b23
Veanty <plmulticraftpl@gmail.com>
2022-08-26 16:40:38 +0200

    Feat: add new component to display item details

---
+++ router/src/ItemDetail.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export function ItemDetail() {
+  useEffect(() => {
+
+  }, []);
+
+  const [item, setItem] = useState([]);
+
+  
+
+  return (
+    <div className="container">
+      <h1>Item</h1>
+    </div>
+  );
+}
