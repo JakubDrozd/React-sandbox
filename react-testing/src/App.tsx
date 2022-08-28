a09c24400d3905d804ed415d67730d27755c1e4d
Veanty <plmulticraftpl@gmail.com>
2022-08-28 15:08:59 +0200

    Refactor: change component structure

--- react-testing/src/App.tsx
+++ react-testing/src/App.tsx
@@ -1,11 +1,8 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  let [ctr, setCtr] = useState(0)
-  useEffect(() => {
-    setCtr(1)
-  },[])
-  return <div data-testid="ctr">{ctr}</div>
+  let [counter, setCounter] = useState(0)
+  return <button data-testid="button" onClick={()=> setCounter(counter + 1)}>{counter}</button>
 }
 
 export default App;
