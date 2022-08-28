0709f451c42d5a1b043c9726d9abbb0ab701ffa2
Veanty <plmulticraftpl@gmail.com>
2022-08-28 15:39:34 +0200

    Feat: delete useless imports

--- react-testing/src/App.test.js
+++ react-testing/src/App.test.js
@@ -1,9 +1,6 @@
 import App from "./App";
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import ReactDOM from "react-dom/client";
-import { act } from "react-dom/test-utils";
 
 it("should tick after one second to new value", () => {
   render(<App></App>);
