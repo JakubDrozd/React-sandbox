9968becb609f94a96b41f7d07eeb0b69fa757560
Veanty <plmulticraftpl@gmail.com>
2022-08-28 15:57:47 +0200

    Refactor: change setTimeout to useFakeTimers and advatnceTimersByTime

--- react-testing/src/App.test.js
+++ react-testing/src/App.test.js
@@ -1,12 +1,14 @@
 import App from "./App";
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 
 it("should tick after one second to new value", () => {
+  jest.useFakeTimers();
   render(<App></App>);
   const timer = screen.getByTestId("timer");
   expect(timer.innerHTML).toBe("0");
-  setTimeout(() => {
-    expect(timer.innerHTML).toBe("1");
+  act(() => {
+    jest.advanceTimersByTime(1000);
   });
 });
