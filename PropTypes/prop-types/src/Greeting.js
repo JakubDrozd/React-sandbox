967a5bc6ab144b3a954842dbf65178a026f779d8
Veanty <plmulticraftpl@gmail.com>
2022-09-18 11:43:39 +0200

    Feat: new PropTypes for Greeting component

--- PropTypes/prop-types/src/Greeting.js
+++ PropTypes/prop-types/src/Greeting.js
@@ -1,13 +1,31 @@
 import { PropTypes } from "prop-types";
 
-Greeting.propTypes = {
-  name: PropTypes.string,
-};
-
-export function Greeting({ name }) {
+export function Greeting({ name, repos }) {
   return (
     <div>
       <h1>Good morning {name}</h1>
+      <h2>List of your repositories:</h2>
+      <ul>
+        {repos.map((project) => {
+          return (
+            <li key={project.name} style={{ listStyle: "none" }}>
+              <h3>
+                {project.index}. {project.name}
+              </h3>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
+
+Greeting.propTypes = {
+  name: PropTypes.string,
+  repos: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      index: PropTypes.number.isRequired,
+    })
+  ),
+};
