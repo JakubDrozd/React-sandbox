b06e7ac1ae44827e86b0e0c9e766a74ec3bda41a
Veanty <plmulticraftpl@gmail.com>
2022-09-18 11:44:12 +0200

    Feat: add new props to Greeting component

--- PropTypes/prop-types/src/App.js
+++ PropTypes/prop-types/src/App.js
@@ -3,7 +3,18 @@ import { Greeting } from "./Greeting";
 function App() {
   return (
     <div>
-      <Greeting name={30}></Greeting>
+      <Greeting
+        name="Jakub"
+        repos={[
+          { name: "project-shopping-car", index: 1 },
+          { name: "react-sandbox", index: 2 },
+          { name: "memory-card", index: 3 },
+          { name: "typescript", index: 4 },
+          { name: "cv-application", index: 5 },
+          { name: "battleships", index: 6 },
+          { name: "js-top", index: 7 },
+        ]}
+      ></Greeting>
     </div>
   );
 }
