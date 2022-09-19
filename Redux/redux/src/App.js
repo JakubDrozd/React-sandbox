a83e5740c5ff5f88258a2cbbfbaa3067fd3d5c01
Veanty <plmulticraftpl@gmail.com>
2022-09-19 18:04:28 +0200

    Feat: add new buttons with count stored in Redux

--- Redux/redux/src/App.js
+++ Redux/redux/src/App.js
@@ -1,5 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./counter";
+
 function App() {
-  return <div></div>;
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>The count is: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
 }
 
 export default App;
