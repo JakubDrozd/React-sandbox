dae88be50edf417b6d4912a5b1d0b3e500df01db
Veanty <plmulticraftpl@gmail.com>
2022-09-20 16:33:16 +0200

    Feat: add new home component

--- Redux/redux/src/Home.js
+++ Redux/redux/src/Home.js
@@ -1,3 +1,12 @@
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
 export default function Home() {
-  return <h1>Home</h1>;
+  const { value, setValue } = useContext(UserContext);
+  return (
+    <div>
+      <h1>Home</h1>
+      {value}
+      <button onClick={() => setValue("Hey!")}>Change value</button>
+    </div>
+  );
 }
