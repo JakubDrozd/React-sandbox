e1fdf550e8dd9c829b806e0a81c6e0775a787d4a
Veanty <plmulticraftpl@gmail.com>
2022-09-20 15:52:55 +0200

    Feat:add newly created components to the website

--- Redux/redux/src/App.js
+++ Redux/redux/src/App.js
@@ -1,5 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement } from "./counter";
+import Section from "./Section";
+import Heading from "./Heading";
 
 function App() {
   const { count } = useSelector((state) => state.counter);
@@ -9,6 +11,24 @@ function App() {
       <h1>The count is: {count}</h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <Section level={1}>
+        <Heading>Title</Heading>
+        <Section level={2}>
+          <Heading>Heading</Heading>
+          <Heading>Heading</Heading>
+          <Heading>Heading</Heading>
+          <Section level={3}>
+            <Heading>Sub-heading</Heading>
+            <Heading>Sub-heading</Heading>
+            <Heading>Sub-heading</Heading>
+            <Section level={4}>
+              <Heading>Sub-sub-heading</Heading>
+              <Heading>Sub-sub-heading</Heading>
+              <Heading>Sub-sub-heading</Heading>
+            </Section>
+          </Section>
+        </Section>
+      </Section>
     </div>
   );
 }
