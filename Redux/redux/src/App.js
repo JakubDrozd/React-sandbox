88107a387af7019f8af585d97721bfbae2ed2f73
Veanty <plmulticraftpl@gmail.com>
2022-09-20 16:16:27 +0200

    Feat: add new routing to the webiste

--- Redux/redux/src/App.js
+++ Redux/redux/src/App.js
@@ -2,34 +2,55 @@ import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement } from "./counter";
 import Section from "./Section";
 import Heading from "./Heading";
+import { Link, BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
 
 function App() {
   const { count } = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   return (
-    <div>
-      <h1>The count is: {count}</h1>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <Section>
-        <Heading>Title</Heading>
+    <Router>
+      <div>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <h1>The count is: {count}</h1>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
         <Section>
-          <Heading>Heading</Heading>
-          <Heading>Heading</Heading>
-          <Heading>Heading</Heading>
+          <Heading>Title</Heading>
           <Section>
-            <Heading>Sub-heading</Heading>
-            <Heading>Sub-heading</Heading>
-            <Heading>Sub-heading</Heading>
+            <Heading>Heading</Heading>
+            <Heading>Heading</Heading>
+            <Heading>Heading</Heading>
             <Section>
-              <Heading>Sub-sub-heading</Heading>
-              <Heading>Sub-sub-heading</Heading>
-              <Heading>Sub-sub-heading</Heading>
+              <Heading>Sub-heading</Heading>
+              <Heading>Sub-heading</Heading>
+              <Heading>Sub-heading</Heading>
+              <Section>
+                <Heading>Sub-sub-heading</Heading>
+                <Heading>Sub-sub-heading</Heading>
+                <Heading>Sub-sub-heading</Heading>
+              </Section>
             </Section>
           </Section>
         </Section>
-      </Section>
-    </div>
+      </div>
+      <Routes>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
