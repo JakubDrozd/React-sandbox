8d981f9fc1f867f7f7689fa8ef8dffecfc666059
Veanty <plmulticraftpl@gmail.com>
2022-09-24 13:42:55 +0200

    Feat: add new library file which contains all styled components

---
+++ HOCs/hocs/src/StyledLib.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Base = styled.div`
+  font-size: 1em;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
