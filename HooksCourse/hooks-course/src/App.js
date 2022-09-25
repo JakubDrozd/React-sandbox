d2fd5396bb8ac8ed37adc8b467fde97307daa2cc
Veanty <plmulticraftpl@gmail.com>
2022-09-25 15:56:12 +0200

    Feat: add new counter state

--- HooksCourse/hooks-course/src/App.js
+++ HooksCourse/hooks-course/src/App.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Base = styled.div`
+  text-align: center;
+`;
+
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const [showText, setShowText] = useState(false);
+  return (
+    <Base>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+          setShowText(!showText);
+        }}
+      >
+        Click Here
+      </button>
+    </Base>
+  );
 }
 
 export default App;
