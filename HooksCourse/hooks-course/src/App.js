bc8c932f4e084b97e6d52b07bedc6da36d766a29
Veanty <plmulticraftpl@gmail.com>
2022-09-25 16:27:55 +0200

    Feat: add useReducer to the component

--- HooksCourse/hooks-course/src/App.js
+++ HooksCourse/hooks-course/src/App.js
@@ -1,24 +1,35 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import styled from "styled-components";
 
 const Base = styled.div`
   text-align: center;
 `;
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1, showText: state.showText };
+    case "toggleShowText":
+      return { count: state.count, showText: !state.showText };
+    default:
+      throw new Error();
+  }
+};
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [showText, setShowText] = useState(false);
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
   return (
     <Base>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <button
         onClick={() => {
-          setCount(count + 1);
-          setShowText(!showText);
+          dispatch({ type: "increment" });
+          dispatch({ type: "toggleShowText" });
         }}
       >
         Click Here
       </button>
+      {state.showText && <p>This is a text</p>}
     </Base>
   );
 }
