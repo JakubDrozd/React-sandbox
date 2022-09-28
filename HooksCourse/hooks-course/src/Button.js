e03733df70ae0009e17b7ae3d1d6d4ae9d84f057
Veanty <plmulticraftpl@gmail.com>
2022-09-28 12:30:43 +0200

    Feat: add imperative handler to the button

--- HooksCourse/hooks-course/src/Button.js
+++ HooksCourse/hooks-course/src/Button.js
@@ -1,13 +1,20 @@
-import { useState } from "react";
-function Button() {
+import { useImperativeHandle } from "react";
+import { useState, forwardRef } from "react";
+
+const Button = forwardRef((props, ref) => {
   const [toggle, setToggle] = useState(false);
 
+  useImperativeHandle(ref, () => ({
+    alterToggle() {
+      setToggle(!toggle);
+    },
+  }));
   return (
     <>
       <button onClick={() => setToggle(!toggle)}>Button from child</button>
       {toggle && <span>Toggle</span>}
     </>
   );
-}
+});
 
 export default Button;
