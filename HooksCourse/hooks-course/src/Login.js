62b561fafb149cb10654bd8310e39c73eeea0295
Veanty <plmulticraftpl@gmail.com>
2022-09-28 14:04:49 +0200

    Refactor: now uses context to access the state

--- HooksCourse/hooks-course/src/Login.js
+++ HooksCourse/hooks-course/src/Login.js
@@ -1,4 +1,9 @@
-const Login = ({ setUsername }) => {
+import { useContext } from "react";
+import { AppContext } from "./App";
+
+const Login = () => {
+  const { setUsername } = useContext(AppContext);
+
   return (
     <div>
       <input
